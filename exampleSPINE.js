94947ebfd7b60496545323f0a4c7e3313b243294
Roberto Solano <rsolano@ddcinternal.com>
2019-03-28 13:55:50 -0600

    123

--- exampleSPINE.js
+++ exampleSPINE.js
@@ -9,22 +9,31 @@ PIXI.loader
     .load(onLoaded);
 
 var params = {
-    alienQuantity: 100
+    alienQuantity: 20
 };
 
 
 var alienList = [];
 // Handle the load completed
 function onLoaded (loader,res) {
-   
     for(var alienIndex = 0; alienIndex < params.alienQuantity; alienIndex++){
         alienList[alienIndex] = new PIXI.spine.Spine(res.alien.spineData);
         alienList[alienIndex].skeleton.setSlotsToSetupPose();
-        
+        alienList[alienIndex].skeleton.setToSetupPose();
+        alienList[alienIndex].update(0);
+        alienList[alienIndex].autoUpdate = false;
         alienList[alienIndex].x = 200 + alienIndex * 5;
         alienList[alienIndex].y = 500 ;
         alienList[alienIndex].state.setAnimation(0, 'death', true);
         app.stage.addChild(alienList[alienIndex]);
     }
-    
+    app.ticker.add(function() {
+        for(var alienIndex = 0; alienIndex < params.alienQuantity; alienIndex++){
+        // update the spine animation, only needed if dragon.autoupdate is set to false
+            alienList[alienIndex].update(0.015); // HARDCODED FRAMERATE!
+            //console.log(alienList);
+        }
+    });
 }
+
+ 
